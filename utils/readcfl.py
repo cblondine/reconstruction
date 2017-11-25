8cae4f789e28f858a1e7a914a49ce07d2dbbc952
cblondine <33984380+cblondine@users.noreply.github.com>
2017-11-25 17:06:29 +0100

    Add files via upload

---
+++ utils/readcfl.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def readcfl(name):
+    # get dims from .hdr
+    h = open(name + ".hdr", "r")
+    h.readline() # skip
+    l = h.readline()
+    h.close()
+    dims = [int(i) for i in l.split( )]
+
+    # remove singleton dimensions from the end
+    n = np.prod(dims)
+    dims_prod = np.cumprod(dims)
+    dims = dims[:np.searchsorted(dims_prod, n)+1]
+
+    # load data and reshape into dims
+    d = open(name + ".cfl", "r")
+    a = np.fromfile(d, dtype=np.complex64, count=n);
+    d.close()
+    return a.reshape(dims, order='F') # column-major
+
